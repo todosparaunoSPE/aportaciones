9d19475a4df98ba618216af6c9d7b6c8b974a557
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-07 13:44:55 -0600

    Update simulados_gastos_medicos_3.py

--- simulados_gastos_medicos_3.py
+++ simulados_gastos_medicos_3.py
@@ -10,6 +10,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Tasa de rendimiento anual y mensual
 tasa_rendimiento_anual = 0.05
 tasa_rendimiento_mensual = tasa_rendimiento_anual / 12
